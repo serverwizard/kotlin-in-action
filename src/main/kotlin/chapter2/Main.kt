4ffc695576adb658f98106cb8d9df9bfbe578cab
홍종완 <serverwizard.h@woowahan.com>
2022-02-12 14:18:05 +0900

    [kotlin-in-action] 2.19 예제

--- src/main/kotlin/chapter2/Main.kt
+++ src/main/kotlin/chapter2/Main.kt
@@ -4,16 +4,15 @@ interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 
-fun eval(e: Expr): Int {
+fun eval(e: Expr): Int =
     if (e is Num) {
-        return e.value
+        e.value
+    } else if (e is Sum) {
+        eval(e.left) + eval(e.right)
+    } else {
+        throw IllegalArgumentException("Unknown expression")
     }
-    if (e is Sum) {
-        return eval(e.left) + eval(e.right)
-    }
-    throw IllegalArgumentException("Unknown expression")
-}
 
 fun main(args: Array<String>) {
-    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+    println(eval(Sum(Num(1), Num(2))))
 }
