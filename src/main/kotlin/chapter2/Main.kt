1e8a3c07776c38e8e5cead351a01f85a2400c6b0
홍종완 <serverwizard.h@woowahan.com>
2022-02-12 14:02:34 +0900

    [kotlin-in-action] 2.13 예제

--- src/main/kotlin/chapter2/Main.kt
+++ src/main/kotlin/chapter2/Main.kt
@@ -1,16 +1,11 @@
 package chapter2
 
-fun getMnemonic(color: Color) =
-    when (color) {
-        Color.RED -> "Richard"
-        Color.ORANGE -> "Of"
-        Color.YELLOW -> "York"
-        Color.GREEN -> "Gave"
-        Color.BLUE -> "Battle"
-        Color.INDIGO -> "In"
-        Color.VIOLET -> "Vain"
-    }
+fun getWarmth(color: Color) = when (color) {
+    Color.RED, Color.ORANGE, Color.YELLOW -> "warm"
+    Color.GREEN -> "neutral"
+    Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
+}
 
 fun main(args: Array<String>) {
-    println(getMnemonic(Color.BLUE))
+    println(getWarmth(Color.ORANGE))
 }
