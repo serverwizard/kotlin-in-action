755e0f63f50e5cc6b1f3393b197830ee9cfbf4cc
홍종완 <serverwizard.h@woowahan.com>
2022-02-12 14:26:25 +0900

    [kotlin-in-action] 2.23 예제

--- src/main/kotlin/chapter2/Main.kt
+++ src/main/kotlin/chapter2/Main.kt
@@ -1,14 +1,14 @@
 package chapter2
 
 fun fizzBuzz(i: Int) = when {
-    i % 15 == 0 -> "FizzBuzz"
-    i % 3 == 0 -> "Fizz"
-    i % 5 == 0 -> "Buzz"
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
     else -> "$i "
 }
 
 fun main(args: Array<String>) {
-    for (i in 1..10) {
+    for (i in 100 downTo 1 step 2) {
         print(fizzBuzz(i))
     }
 }
