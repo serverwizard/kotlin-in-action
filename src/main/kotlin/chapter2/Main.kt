b66022611ccc3c01a12d0ff353dde2f6ebbbc107
홍종완 <serverwizard.h@woowahan.com>
2022-02-12 14:32:37 +0900

    [kotlin-in-action] 2.25 예제

--- src/main/kotlin/chapter2/Main.kt
+++ src/main/kotlin/chapter2/Main.kt
@@ -1,18 +1,9 @@
 package chapter2
 
-import java.util.TreeMap
+fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+fun isNotDigit(c: Char) = c !in '0'..'9'
 
 fun main(args: Array<String>) {
-    val binaryReps = TreeMap<Char, String>()
-
-    for (c in 'A'..'F') {
-        val binary = Integer.toBinaryString(c.code)
-        println(binary)
-        binaryReps[c] = binary
-    }
-
-    for((letter, binary) in binaryReps) {
-        println("$letter = $binary")
-    }
+    println(isLetter('q'))
+    println(isNotDigit('x'))
 }
-
