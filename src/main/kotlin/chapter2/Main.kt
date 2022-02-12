8609006e79f39c6659d9754010832049160d9567
홍종완 <serverwizard.h@woowahan.com>
2022-02-12 14:12:00 +0900

    [kotlin-in-action] 2.16 예제

--- src/main/kotlin/chapter2/Main.kt
+++ src/main/kotlin/chapter2/Main.kt
@@ -9,14 +9,20 @@ import chapter2temp.Color.RED
 import chapter2temp.Color.VIOLET
 import chapter2temp.Color.YELLOW
 
-fun mix(c1: Color, c2: Color) =
-    when (setOf(c1, c2)) {
-        setOf(RED, YELLOW) -> ORANGE
-        setOf(YELLOW, BLUE) -> GREEN
-        setOf(BLUE, VIOLET) -> INDIGO
-        else -> throw Exception("Dirty color")
+fun mixOptimized(c1: Color, c2: Color) =
+    when {
+        (c1 == RED && c2 == YELLOW) ||
+            (c2 == YELLOW && c2 == RED) -> ORANGE
+
+        (c1 == YELLOW && c2 == BLUE) ||
+            (c1 == BLUE && c2 == YELLOW) -> GREEN
+
+        (c1 == BLUE && c2 == VIOLET) ||
+            (c1 == VIOLET && c2 == BLUE) -> INDIGO
+
+        else -> throw Exception("Dirty Color")
     }
 
 fun main(args: Array<String>) {
-    println(mix(BLUE, YELLOW))
+    println(mixOptimized(BLUE, YELLOW))
 }
