6cb904486f74ea4ae91f8fbba140f3ee088110c7
홍종완 <serverwizard.h@woowahan.com>
2022-02-12 14:39:26 +0900

    [kotlin-in-action] 2.28 예제

--- src/main/kotlin/chapter2/Main.kt
+++ src/main/kotlin/chapter2/Main.kt
@@ -3,18 +3,16 @@ package chapter2
 import java.io.BufferedReader
 import java.io.StringReader
 
-fun readNumber(reader: BufferedReader): Int? {
-    try {
-        val line = reader.readLine()
-        return Integer.parseInt(line)
+fun readNumber(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
     } catch (e: NumberFormatException) {
-        return null
-    } finally {
-        reader.close()
+        return
     }
+    println(number) // kotlin.Unit
 }
 
 fun main(args: Array<String>) {
-    var reader = BufferedReader(StringReader("239"))
+    var reader = BufferedReader(StringReader("not a number"))
     println(readNumber(reader))
 }
