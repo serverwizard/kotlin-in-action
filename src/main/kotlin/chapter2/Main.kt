148eb349803c62eef369f00c8d154b971dd756f6
홍종완 <serverwizard.h@woowahan.com>
2022-02-12 14:30:36 +0900

    [kotlin-in-action] 2.24 예제

--- src/main/kotlin/chapter2/Main.kt
+++ src/main/kotlin/chapter2/Main.kt
@@ -1,14 +1,18 @@
 package chapter2
 
-fun fizzBuzz(i: Int) = when {
-    i % 15 == 0 -> "FizzBuzz "
-    i % 3 == 0 -> "Fizz "
-    i % 5 == 0 -> "Buzz "
-    else -> "$i "
-}
+import java.util.TreeMap
 
 fun main(args: Array<String>) {
-    for (i in 100 downTo 1 step 2) {
-        print(fizzBuzz(i))
+    val binaryReps = TreeMap<Char, String>()
+
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.code)
+        println(binary)
+        binaryReps[c] = binary
+    }
+
+    for((letter, binary) in binaryReps) {
+        println("$letter = $binary")
     }
 }
+
